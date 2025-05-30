bd2a48d2b9d75f1fdb72261a423f66fc3713d82a
Davide Tedesco <d.tedesco@metodafinance.it>
2025-05-30 16:43:05 +0200

    WIP LOG => Implementato per tutto PolarDriveInitDB.Cli

--- backend/PolarDriveInitDB.Cli/Program.cs
+++ backend/PolarDriveInitDB.Cli/Program.cs
@@ -1,23 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PolarDrive.Data.DbContexts;
+using PolarDrive.Data.Entities;
 
 Console.WriteLine("🚀 Starting PolarDrive DB initialization...");
 
 using var db = new PolarDriveDbContextFactory().CreateDbContext(args);
+var logger = new PolarDriveLogger(db);
 
-// 1. Applica tutte le migrations
-// 1.1 MIGRATION DA ABILITARE QUANDO SI ANDRA' IN PRODUZIONE
-// - Quando vorrai tornare a usare le migration (es. per staging o produzione):
-// - Esegui bash: dotnet ef migrations add InitialProductionSchema
-// - Torna a usare: await db.Database.MigrateAsync();
-// Console.WriteLine("✅ Migrations applied.");
+try
+{
+    await logger.Info("PolarDriveInitDB.Cli", "Starting DB initialization process");
 
-// 1. Check su Delete / Create DB
-await db.Database.EnsureDeletedAsync();
-await db.Database.EnsureCreatedAsync();
-// Console.WriteLine("✅ Create DONE!");
+    // 1. Apply all migrations (PRODUCTION ONLY)
+    // await db.Database.MigrateAsync(); // Uncomment when switching to production
+    // await logger.Info("PolarDriveInitDB.Cli", "Migrations applied successfully");
 
-// 2. Esegui script SQL extra
-await DbInitHelper.RunDbInitScriptsAsync(db);
+    // 1. Delete / Create DB (DEV only)
+    await db.Database.EnsureDeletedAsync();
+    await logger.Info("PolarDriveInitDB.Cli", "Database deleted successfully");
 
-Console.WriteLine("🏁 Final Initialization Done.");
+    await db.Database.EnsureCreatedAsync();
+    await logger.Info("PolarDriveInitDB.Cli", "Database created successfully");
+
+    // 2. Execute extra SQL scripts
+    await DbInitHelper.RunDbInitScriptsAsync(db);
+    await logger.Info("PolarDriveInitDB.Cli", "DB initialization scripts executed successfully");
+
+    Console.WriteLine("🏁 Final Initialization Done.");
+}
+catch (Exception ex)
+{
+    await logger.Error(
+        "PolarDriveInitDB.Cli",
+        "An error occurred during the database initialization process",
+        $"Exception: {ex}"
+    );
+
+    Console.WriteLine($"❌ FATAL ERROR during initialization: {ex.Message}");
+}
