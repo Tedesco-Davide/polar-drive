c67db6061bbe4e63ad8dc3de8b2b156d11065b5b
Davide Tedesco <support@datapolar.dev>
2025-11-28 19:49:03 +0100

    fix pulizia codice inutilizzato

--- frontend/utils/date.ts
+++ frontend/utils/date.ts
@@ -10,11 +10,3 @@ export const formatDateToSave = (date: Date) => format(date, "yyyy-MM-dd");
 
 export const formatOutageDateTimeToSave = (date: Date): string =>
   format(date, "yyyy-MM-dd'T'HH:mm");
-
-export const formatOutageDateToDisplay = (
-  isoDate: string | null | undefined
-) => {
-  if (!isoDate) return "";
-  const parsedDate = parseISO(isoDate);
-  return isValid(parsedDate) ? format(parsedDate, "dd/MM/yyyy HH:mm") : "";
-};
